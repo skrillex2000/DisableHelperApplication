ecb5cae813ea8a437c7776812187ac31d37c46c2
nanda <nanda@DESKTOP-S94QA96>
2024-03-25 10:40:41 +0530

    Add project files.

---
+++ DisableHelperApplication/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DisableHelperApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(203, 61);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(520, 259);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            this.richTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Text-To-Speech",
+            "Speech -To- Text"});
+            this.comboBox1.Location = new System.Drawing.Point(203, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(240, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Converter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(345, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Status";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(42, 71);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "StoreToLocal";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ DisableHelperApplication/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Globalization;
+using System.IO;
+
+namespace DisableHelperApplication
+{
+    public partial class Form1 : Form
+    {
+        //
+        string texttoSpeakOut;
+        string InputSpeech;
+
+        static bool shiftisPressed;
+        FileStream fs;
+
+
+
+
+
+
+
+        //
+        public Form1()
+        {
+            InitializeComponent(); 
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            try
+            {
+               await ConvertingTask();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+            button1.Enabled = true;
+        }
+
+        private async Task ConvertingTask()
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                texttoSpeakOut= richTextBox1.Text;
+                await Task.Run(ReadTheLines);
+            }
+            else
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                fs = File.Create(path+"\\Speech.txt");
+                 
+                await Task.Run(ListenToSpeech);
+                richTextBox1.Text = InputSpeech; 
+            }
+       
+        }
+
+        void ReadTheLines()
+        {
+            SpeechSynthesizer script = new SpeechSynthesizer();
+            script.SelectVoiceByHints(VoiceGender.Neutral, VoiceAge.Adult);
+          //  script.Speak(texttoSpeakOut.ToString());
+            if (true == checkBox1.Checked)
+            {
+                script.SetOutputToWaveFile(@"C:\Users\nanda\Downloads\MyWavFile.wav");
+            }
+            script.Speak(texttoSpeakOut.ToString());
+           
+        }
+
+        void ListenToSpeech()
+        {
+            SpeechRecognitionEngine spe =  new SpeechRecognitionEngine();
+            Grammar words = new DictationGrammar();
+            spe.LoadGrammar(words);
+            spe.SetInputToDefaultAudioDevice();
+            RecognitionResult result = spe.Recognize();
+            InputSpeech = result.Text;
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.ShiftKey)shiftisPressed = true;
+        }
+
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter && shiftisPressed == false)
+            {
+                    button1.PerformClick();
+            }
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ShiftKey) shiftisPressed = false;
+        }
+    }
+}
